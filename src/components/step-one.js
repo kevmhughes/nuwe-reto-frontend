b781643b0ff0304d4af85ed4d0f83c802971fc1a
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-06-14 17:41:10 +0200

    Add handleHover methods, and button icons

--- src/components/step-one.js
+++ src/components/step-one.js
@@ -1,22 +1,53 @@
+/* eslint-disable no-console */
+/* eslint-disable react/no-access-state-in-setstate */
+/* eslint-disable no-undef */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-use-before-define */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight, faSquare } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowRight, faSquare, faBriefcase, faUser,
+} from '@fortawesome/free-solid-svg-icons';
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import { SidePanel } from './side-panel';
 import './step-one.css';
 
 export class StepOne extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hovered: true,
+      hoveredTwo: true,
+    };
+    this.handleHover = this.handleHover.bind(this);
+    this.handleHoverTwo = this.handleHoverTwo.bind(this);
+  }
+
+  handleHover() {
+    this.setState({
+      hovered: !this.state.hovered,
+    });
+  }
+
+  handleHoverTwo() {
+    this.setState({
+      hoveredTwo: !this.state.hoveredTwo,
+    });
+  }
+
     continue = (e) => {
       e.preventDefault();
       this.props.nextStep();
     }
 
     render() {
+      const btnClass = this.state.hovered ? 'square' : 'square-two';
+      const btnClassTwo = this.state.hovered ? 'awesome-arrow' : 'awesome-arrow-two';
+      const btnClassThree = this.state.hoveredTwo ? 'square' : 'square-two';
+      const btnClassFour = this.state.hoveredTwo ? 'awesome-arrow' : 'awesome-arrow-two';
       return (
         <div className="flex-container">
           <SidePanel />
@@ -63,28 +94,36 @@ export class StepOne extends Component {
                 onClick={this.continue}
                 className="step-one-button"
                 style={{ height: '80px' }}
+                onMouseEnter={this.handleHoverTwo}
+                onMouseLeave={this.handleHoverTwo}
               >
-                <div className="step-one-star"><FontAwesomeIcon icon={faSquare} className="square" /></div>
+                <div className="step-one-star">
+                  <FontAwesomeIcon icon={faSquare} className={btnClassThree} />
+                  <FontAwesomeIcon icon={faBriefcase} className="briefcase" />
+                </div>
                 <div className="button-right">
                   <p className="step-one-button-text">Developers</p>
                   <p className="step-one-button-text-two">Cuenta personas para entrar en el mundo dev</p>
                 </div>
-                <div className="awesome-arrow"><FontAwesomeIcon icon={faArrowRight} /></div>
+                <div className={btnClassFour}><FontAwesomeIcon icon={faArrowRight} /></div>
               </Button>
               <Button
                 variant="outline-primary"
                 onClick={this.continue}
                 className="step-one-button"
                 style={{ height: '80px' }}
+                onMouseEnter={this.handleHover}
+                onMouseLeave={this.handleHover}
               >
                 <div className="step-one-star">
-                  <FontAwesomeIcon icon={faSquare} className="square" />
+                  <FontAwesomeIcon icon={faSquare} className={btnClass} />
+                  <FontAwesomeIcon icon={faUser} className="user" />
                 </div>
                 <div className="button-right">
                   <p className="step-one-button-text">Business</p>
                   <p className="step-one-button-text-two">Tienes o perteneces a una compañia</p>
                 </div>
-                <div className="awesome-arrow"><FontAwesomeIcon icon={faArrowRight} /></div>
+                <div className={btnClassTwo}><FontAwesomeIcon icon={faArrowRight} /></div>
               </Button>
             </div>
           </div>
