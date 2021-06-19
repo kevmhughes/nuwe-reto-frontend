53c675fdb35237ed777edc730d5d2313cd6ebecf
Kevin Hughes <48656356+kevmhughes@users.noreply.github.com>
2021-06-19 02:59:45 +0200

    Update step-two.js

--- src/components/step-two.js
+++ src/components/step-two.js
@@ -155,7 +155,7 @@ export class StepTwo extends Component {
                     <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png" alt="Google logo" style={{ height: '15px' }} />
                   </div>
                   {' '}
-                  Registrate con Google
+                  Regístrate con Google
                 </Button>
                 <br />
               </Form>
